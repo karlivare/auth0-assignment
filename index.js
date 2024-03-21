535ee96896cf4ba7472656daf46ecc85af0679b2
Karli Vare <karlivare@Karlis-MacBook-Pro.local>
2024-03-21 15:15:14 -0400

    trying to make auth0 work

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const config = {
     secret: process.env.AUTH0_SECRET,
     baseURL: process.env.SITE_URL || 'http://localhost:3000',
     clientID: process.env.AUTH0_CLIENT_ID,
-    issuerBaseURL: 'https://bdaley.auth0.com'
+    issuerBaseURL: 'https://dev-vhsjwh4xnrfu0k3m.us.auth0.com'
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
